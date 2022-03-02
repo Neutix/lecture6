462e3b2bd74f1517ac3da7335529a270a2de3c1a
Neutix <martin.wedvik@gmail.com>
2022-03-02 22:17:41 +0100

    random question from server.js

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {randomQuestion} from "./questions";
+
+const app = express();
+
+app.get("/question/random", (req, res) => {
+    const question = randomQuestion();
+    res.json(question)
+})
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log(`server started on http://localhost:${server.address().port}`);
+});
